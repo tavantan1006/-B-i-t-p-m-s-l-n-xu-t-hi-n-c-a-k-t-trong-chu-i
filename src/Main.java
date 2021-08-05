631818a17b72369ef39ec663e68c3a39d36d2676
Tấn tạ <tavantan100698@gmail.com>
2021-08-05 10:32:25 +0700

    Kiem tra nam nhuan

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+        boolean isLeapYear = false;
 
-        System.out.print("Which month that you want to count days? ");
-        int month = scanner.nextInt();
-        String daysInMonth;
-        switch (month) {
-            case 2:
-                daysInMonth = "28 or 29";
-                break;
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                daysInMonth = "31";
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                daysInMonth = "30";
-                break;
-            default:
-                daysInMonth = "";
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if (isDivisibleBy400) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
         }
 
-        if (!daysInMonth.equals("")) System.out.printf("The month '%d' has %s days!", month, daysInMonth);
-        else System.out.print("Invalid input!");
+        if (isLeapYear) {
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
     }
 }
