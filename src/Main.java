2858ef88feb64e90ce76c50ece62922b048e0ed5
Tấn tạ <tavantan100698@gmail.com>
2021-08-06 13:42:08 +0700

    MEnu

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int a;
-        int b;
+        int choice = -1;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a: ");
-        a = input.nextInt();
-        System.out.println("Enter b: ");
-        b = input.nextInt();
-        a = Math.abs(a);
-        b = Math.abs(b);
-        if (a == 0 || b == 0) {
-            System.out.println("No greatest common factor");
+        while(choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Draw the triangle");
+            System.out.println("2. Draw the square");
+            System.out.println("3. Draw the rectangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Draw the triangle");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");
+                    break;
+                case 2:
+                    System.out.println("Draw the square");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("Draw the rectangle");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
         }
-        while (a != b) {
-            if (a > b)
-                a = a - b;
-            else
-                b = b - a;
-        }
-        System.out.println("Greatest common factor: " + a);
     }
 }
