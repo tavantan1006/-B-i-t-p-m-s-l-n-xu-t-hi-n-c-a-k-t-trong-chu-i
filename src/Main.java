091f24181fb33cb24ccba371b0a63a53c8ba4011
Tấn tạ <tavantan100698@gmail.com>
2021-08-06 13:24:04 +0700

    is_Prime

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float rate = 23000;
-        System.out.println("Nhập số tiền cần quy đổi : ");
-        float usd = scanner.nextFloat();
-        System.out.println("Result : "+usd*rate + "VND");
-
+        System.out.print("Enter a number:");
+        int number = scanner.nextInt();
+        if (number < 2){
+            System.out.println(number + " is not a prime");
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(number + " is a prime");
+            else
+                System.out.println(number + " is not a prime");
+        }
     }
 }
