e1ec6af832445fc09c3f5f798626ddcab11caf7a
Tấn tạ <tavantan100698@gmail.com>
2021-08-09 14:43:06 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice = -1;
-        while (choice != 0){
+        while (choice != 4) {
             System.out.println("Menu");
-            System.out.println("1. Draw the triangle");
-            System.out.println("2. Draw the square");
-            System.out.println("3. Draw the rectangle");
-            System.out.println("0. Exit");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle");
+            System.out.println("3. Print isosceles triangle");
+            System.out.println("4. Exit");
             System.out.println("Enter your choice  : ");
             choice = sc.nextInt();
-            switch (choice){
+            switch (choice) {
                 case 1:
                     for (int i = 0; i < 4; i++) {
-                        for (int j = 0; j < 4; j++) {
-                            if (j >= i){
-                                System.out.print("*");
-                            }
+                        for (int j = 0; j < 6; j++) {
+                            System.out.print("*");
                         }
                         System.out.println();
                     }
@@ -26,18 +24,21 @@ public class Main {
                 case 2:
                     for (int i = 0; i < 4; i++) {
                         for (int j = 0; j < 4; j++) {
-                                System.out.print(" *");
+                            if (j<= i){System.out.print(" *");}
                         }
                         System.out.println();
                     }
                     break;
                 case 3:
                     for (int i = 0; i < 4; i++) {
-                        for (int j = 0; j < 6; j++) {
-                            System.out.print("*");
+                        for (int j = 0; j < 4; j++) {
+                            if (j>= i){System.out.print(" *");}
                         }
                         System.out.println();
                     }
+                    break;
+                case 4:
+                    System.out.println("Thoát ra ngoài !!");
                     break;
                 default:
                     System.out.println("No choice");
