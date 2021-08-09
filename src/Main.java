cf48d27f55422dd0a2b93ab95543b42e798d38d9
Tấn tạ <tavantan100698@gmail.com>
2021-08-09 15:11:18 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập số nguyên tố cần hiển thị : ");
-        int numberInput = sc.nextInt();
-        int count = 0;
-        for (int i = 2; count < numberInput; i++) {
+        for (int i = 2; i < 100; i++) {
             if (checkIsPrime(i) == true){
                 System.out.println(i);
-                count ++;
             }
         }
     }
