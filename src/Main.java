62ac35878e90374ed71b3ded76289ed8ac91aa39
Tấn tạ <tavantan100698@gmail.com>
2021-08-09 14:36:07 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double money = 1.0;
-        int month = 1;
-        double interestRate = 1.0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter investment amount: ");
-        money = input.nextDouble();
-        System.out.println("Enter number of months: ");
-        month = input.nextInt();
-        System.out.println("Enter annual interest rate in percentage: ");
-        interestRate = input.nextDouble();
-        double totalInterest = 0;
-        for(int i = 1; i <= month; i++){
-            totalInterest += (money + totalInterest )* (interestRate/100)/12;
+        Scanner sc = new Scanner(System.in);
+        int choice = -1;
+        while (choice != 0){
+            System.out.println("Menu");
+            System.out.println("1. Draw the triangle");
+            System.out.println("2. Draw the square");
+            System.out.println("3. Draw the rectangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice  : ");
+            choice = sc.nextInt();
+            switch (choice){
+                case 1:
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 4; j++) {
+                            if (j >= i){
+                                System.out.print("*");
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 4; j++) {
+                                System.out.print(" *");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 6; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                default:
+                    System.out.println("No choice");
+            }
+
         }
-        System.out.println("Total of interest: " + totalInterest);
     }
 }
