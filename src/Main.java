0fdc64636e2c50ea3ad13d4ff50ac865fe19b9e5
Tấn tạ <tavantan100698@gmail.com>
2021-08-16 21:34:36 +0700

    Tim Max

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[Thực hành] Tìm giá trị trong mảng");
+        System.out.println("[Thực hành] Tìm giá trị Max trong mảng");
         Scanner sc = new Scanner(System.in);
         int size;
-        String [] arrayStudent;
+        int [] arrayA;
         do {
             System.out.println("Enter a size : ");
             size = sc.nextInt();
@@ -13,35 +13,22 @@ public class Main {
                 System.out.println("size < 20");
             }
         }while (size > 20);
-        sc.nextLine();
-        arrayStudent = new String[size];
+        arrayA = new int[size];
         int i = 0;
         while (i < size){
             System.out.println("Enter element array ["+i+"] = ");
-            arrayStudent[i] = sc.nextLine();
+            arrayA[i] = sc.nextInt();
             i++;
         }
-
-        System.out.println("Show value of Elements : ");
-        for (int j = 0; j < arrayStudent.length; j++) {
-            System.out.print(arrayStudent[j] + "\t");
-        }
-
-        System.out.println();
-        String name ="";
-        System.out.println("Enter name you need to find : ");
-        name = sc.nextLine();
-        boolean check = false;
-        for (int j = 0; j < arrayStudent.length; j++) {
-            if (arrayStudent[j].equals(name)){
-                System.out.println("arrayStudent ["+j+"] = "+name);
-                check = true;
-                break;
+        System.out.println("Max in Arrays : ");
+        int max;
+        max = arrayA[0];
+        for (int j = 0; j < arrayA.length; j++) {
+            if (max < arrayA[j]){
+                max = arrayA[j];
             }
         }
-        if (check == false){
-            System.out.println("No find to Name you need find ");
-        }
+        System.out.print(max);
 
 
     }
