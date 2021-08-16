acf9a2de86f63356aefb5379b39dea9d83364079
Tấn tạ <tavantan100698@gmail.com>
2021-08-16 21:15:22 +0700

    [Thực hành] Đảo ngược các phần tử của mảng

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("[Thực hành] Đảo ngược các phần tử của mảng");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập số nguyên tố cần hiển thị : ");
-        for (int i = 2; i < 100; i++) {
-            if (checkIsPrime(i) == true){
-                System.out.println(i);
+        int size;
+        int [] arrayA;
+        do {
+            System.out.println("Enter a size : ");
+            size = sc.nextInt();
+            if (size > 20){
+                System.out.println("size < 20");
             }
+        }while (size > 20);
+        arrayA = new int[size];
+        int i = 0;
+        while (i < size){
+            System.out.println("Enter element array ["+i+"] = ");
+            arrayA[i] = sc.nextInt();
+            i++;
         }
-    }
-    public static boolean checkIsPrime(int number){
-        boolean check = true;
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number%i == 0){
-                check = false;
-                break;
-            }
+        System.out.println("Show value of Elements : ");
+        for (int j = 0; j < arrayA.length; j++) {
+            System.out.print(arrayA[j] + "\t");
         }
-        return check;
-    }
+        for (int j = 0; j < arrayA.length/2; j++) {
+            int temp =  arrayA[j];
+            arrayA[j] = arrayA[size - 1 - j];
+            arrayA[size - 1 - j] = temp;
+        }
+        System.out.println();
+        System.out.println("Reverse array : ");
+        for (int j = 0; j < arrayA.length; j++) {
+            System.out.print(arrayA[j] + "\t");
+        }
 
+
+    }
 }
