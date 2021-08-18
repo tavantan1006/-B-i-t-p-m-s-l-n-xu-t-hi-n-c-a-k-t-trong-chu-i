5878da23088e3e1044e596eb6212fc5c9d87dd0e
Tấn tạ <tavantan100698@gmail.com>
2021-08-18 14:56:43 +0700

    Tong duong cheo

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[*Bài tập] Tính tổng các số ở một cột xác định");
+        System.out.println("[*Bài tập] Mảng hai chiều - tính tổng các số ở đường chéo chính của ma trận vuông");
         System.out.println("Array : ");
-        int[][] arrayA = {{1,2,3,4,0,0,0,0,0},{1,2,3,4,5}};
+        int[][] arrayA = {{1,2,3,4,0},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
         System.out.println("Mảng 1 : ");
-        int lengMax = 0;
+        int tong = 0;
         for (int i = 0; i < arrayA.length; i++) {
-            if (lengMax < arrayA[i].length){
-                lengMax = arrayA[i].length;
-            }
             for (int j = 0; j < arrayA[i].length; j++) {
                 System.out.print(arrayA[i][j] + "\t");
+                if (j==i){
+                    tong+= arrayA[i][j];
+                }
             }
             System.out.println();
         }
-        Scanner sc = new Scanner(System.in);
-        int col;
-        int total = 0;
-        do {
-            System.out.println("Nhập cột cần tính tổng : ");
-             col = sc.nextInt();
-             if (col < 1 || col > lengMax){
-                 System.out.println("Không được vượt quá độ dài cột của mảng !! ");
-             }
-        }while (col < 1 || col > lengMax);
-        for (int i = 0; i < arrayA.length; i++) {
-            for (int j = 0; j < arrayA[i].length; j++) {
-                if (j == col-1){
-                    total+= arrayA[i][j];
-                }
-            }
-        }
-        System.out.println("Tổng giá trị cột "+col+ ": "+total);
+        System.out.println("Tổng đường chéo chính : " + tong);
+
     }
 }
