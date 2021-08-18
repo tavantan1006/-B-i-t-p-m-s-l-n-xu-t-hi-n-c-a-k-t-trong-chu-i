7fb4496b1d0821172524ad20df02ea1defa5abcd
Tấn tạ <tavantan100698@gmail.com>
2021-08-18 14:28:49 +0700

    Tim max Mang 2 chieu

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[Bài tập] Gộp mảng");
+        System.out.println("[Bài tập] Tìm phần tử lớn nhất trong mảng hai chiều");
         System.out.println("Array : ");
-        int[] arrayA = {1,2,3,4,0,0,0,0,0};
-        int[] arrayB = {5,6,7,8,0,0,0,0,0};
-        int[] arrayC = new int[arrayA.length+arrayB.length];
+        int[][] arrayA = {{1,2,3,4,0,0,0,0,0},{1,2,3,4,5}};
         System.out.println("Mảng 1 : ");
+        int max = arrayA[0][0];
         for (int i = 0; i < arrayA.length; i++) {
-            System.out.print(arrayA[i] + "\t");
-        }
-        System.out.println();
-        System.out.println("Mảng 2 : ");
-        for (int i = 0; i < arrayB.length; i++) {
-            System.out.print(arrayB[i] + "\t");
-        }
-        System.out.println();
-        System.out.println("Mảng 3 : ");
-        for (int i = 0; i < arrayC.length; i++) {
-            if (i <=  arrayA.length-1){
-                arrayC[i] = arrayA[i];
-            }else {
-                arrayC[i] = arrayB[i-arrayA.length];
+            for (int j = 0; j < arrayA[i].length; j++) {
+                if (max < arrayA[i][j]){
+                    max = arrayA[i][j];
+                }
+                System.out.print(arrayA[i][j] + "\t");
             }
-            System.out.print(arrayC[i] + "\t");
+            System.out.println();
         }
+        System.out.println("Phần tử max : " + max);
     }
 }
