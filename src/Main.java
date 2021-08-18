72cf6d68d92e4291d0b1c005d459dcdafb2856d5
Tấn tạ <tavantan100698@gmail.com>
2021-08-18 14:00:52 +0700

    Themphantumang

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[Thực hành] Tìm giá trị nhỏ nhất trong mảng sử dụng phương thức");
-        int [] arrayA = {4, 5, 6, 78, 9, 6, 45};
-        System.out.println("Show Array : ");
+        System.out.println("[Bài tập] Thêm phần tử vào mảng");
+        System.out.println("Array : ");
+        int[] arrayA = {1,2,3,4,0,0,0,0,0};
         for (int i = 0; i < arrayA.length; i++) {
             System.out.print(arrayA[i] + "\t");
         }
-        System.out.println();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Element need delete in Array : ");
-        int element = sc.nextInt();
-        int count = arrayA.length;
-        int check = 0;
-        for (int i = 0; i < arrayA.length; i++) {
-            if (element == arrayA[i]){
-                count--;
-            }
-        }
-        int [] arrayNew = new int[count];
-        for (int i = 0; i < arrayNew.length; i++) {
-            if (element == arrayA[i+check]){
-                check ++;
-                arrayNew[i] = arrayA[i+check];
-                continue;
+        System.out.println();
+        int index;
+        do {
+            System.out.println("Nhập vị trí thêm phần tử : ");
+             index = sc.nextInt();
+             if (index <= -1 || index >= arrayA.length-1){
+                 System.out.println("Không chèn được vào phần tử mảng !!!");
+             }
+        }while (index <= -1 || index >= arrayA.length-1);
+        System.out.println("Nhập Giá trị phần tử cần thêm : ");
+        int valueIndex = sc.nextInt();
+        for (int i = arrayA.length-1; i >= index; i--) {
+            if (i == index){
+                arrayA[i] = valueIndex;
             }else {
-                arrayNew[i] = arrayA[i+check];
+                arrayA[i] = arrayA[i-1];
             }
         }
-        System.out.println("New array : ");
-        for (int i = 0; i < arrayNew.length; i++) {
-            System.out.print(arrayNew[i] + "\t");
+        System.out.println("Mảng mới : ");
+        for (int i = 0; i < arrayA.length; i++) {
+            System.out.print(arrayA[i] + "\t");
         }
-
     }
+
 }
