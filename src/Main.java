295850547952815c6a02043415cfa58586fe023f
Tấn tạ <tavantan100698@gmail.com>
2021-08-18 14:52:04 +0700

    Tinh tong cot

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int size;
-        int[] array;
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("[*Bài tập] Tính tổng các số ở một cột xác định");
+        System.out.println("Array : ");
+        int[][] arrayA = {{1,2,3,4,0,0,0,0,0},{1,2,3,4,5}};
+        System.out.println("Mảng 1 : ");
+        int lengMax = 0;
+        for (int i = 0; i < arrayA.length; i++) {
+            if (lengMax < arrayA[i].length){
+                lengMax = arrayA[i].length;
+            }
+            for (int j = 0; j < arrayA[i].length; j++) {
+                System.out.print(arrayA[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        Scanner sc = new Scanner(System.in);
+        int col;
+        int total = 0;
         do {
-            System.out.print("Enter a size:");
-            size = scanner.nextInt();
-            if (size > 30)
-                System.out.println("Size should not exceed 30");
-        } while (size > 30);
-        array = new int[size];
-        int i = 0;
-        while (i < array.length) {
-            System.out.print("Enter a mark for student " + (i + 1) + ": ");
-            array[i] = scanner.nextInt();
-            i++;
+            System.out.println("Nhập cột cần tính tổng : ");
+             col = sc.nextInt();
+             if (col < 1 || col > lengMax){
+                 System.out.println("Không được vượt quá độ dài cột của mảng !! ");
+             }
+        }while (col < 1 || col > lengMax);
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayA[i].length; j++) {
+                if (j == col-1){
+                    total+= arrayA[i][j];
+                }
+            }
         }
-        int count = 0;
-        System.out.print("List of mark: ");
-        for (int j = 0; j < array.length; j++) {
-            System.out.print(array[j] + "\t");
-            if (array[j] >= 5 && array[j] <= 10)
-                count++;
-        }
-        System.out.print("\n The number of students passing the exam is " + count);
+        System.out.println("Tổng giá trị cột "+col+ ": "+total);
     }
 }
